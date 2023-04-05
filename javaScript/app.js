250ec5e3b9b160ecc6ff1eb2742630545c32c776
Ayanbanerjee93 <ayanbanerjee.1893@gmail.com>
2023-04-05 10:59:32 +0530

    custom css & more js added

--- javaScript/app.js
+++ javaScript/app.js
@@ -1,3 +1,24 @@
+
+document.getElementById('triangle-btn').addEventListener('click', function(){
+
+    const triangleField= document.getElementById('triangle-base-field');
+    const triangleFieldString=triangleField.value;
+    const triangleNumberField= parseInt(triangleFieldString);
+    
+    const triangleHeightField=document.getElementById('triangle-height-field');
+    const triangleHeightFieldString=triangleHeightField.value;
+    const triangleHeightNumber= parseInt(triangleHeightFieldString);
+    
+    const totalArea= 0.5*triangleNumberField*triangleHeightNumber;
+
+   const triangleResult= document.getElementById('triangle-result');
+   triangleResult.innerText= totalArea;
+
+    
+});
+  
+    
+    
 document.getElementById('triangle-btn').addEventListener('click', function(){
     const triangleField= document.getElementById('triangle-base-field');
     const triangleFieldString=triangleField.value;
@@ -9,9 +30,10 @@ document.getElementById('triangle-btn').addEventListener('click', function(){
     
     const totalArea= 0.5*triangleNumberField*triangleHeightNumber;
 
-    console.log(totalArea,'cm')
+   
+})
+
+
 
     
 
-
-})
